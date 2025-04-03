5de15f328b8071cd5c6bdf8dd267ee81f409c273
inevitablegs <inevitable3101@gmail.com>
2025-04-03 21:28:36 +0530

    home

--- home/views.py
+++ home/views.py
@@ -8,3 +8,21 @@ def custom_logout(request):
 
 def home(request):
     return render(request, 'home/home.html')
+
+def about(request):
+    return render(request, 'home/about.html')
+
+def contact(request):
+    return render(request, 'home/contact.html')
+
+def terms(request):
+    return render(request, 'home/terms.html')
+
+def privacy(request):
+    return render(request, 'home/privacy.html')
+
+def faq(request):
+    return render(request, 'home/faq.html')
+
+def services(request):
+    return render(request, 'home/services.html')
